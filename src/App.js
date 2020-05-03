f57f795af63cb2d38a77d4c24130913d2bfed4d7
Mangaldeep Das <39870866+liku88@users.noreply.github.com>
2020-05-03 22:07:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Jumbo from './Jumbotron'
+import Feat from './Feature'
+import Get from './GetTouch'
+import Footer from './Footer'
+
+const App = () => {
+    return(
+        <div>
+    <Navbar />
+    <Jumbo />
+    <Feat/>
+    <Get/>
+    <Footer/>
+    </div>
+    )
+}
+
+export default App
